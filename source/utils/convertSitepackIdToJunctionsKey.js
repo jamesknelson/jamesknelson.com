299eb5f7c51f0f399c2be5f331617cd3a6383747
jamesknelson <james@jamesknelson.com>
2017-03-10 18:34:10 +0900

    add background color

--- source/utils/convertSitepackIdToJunctionsKey.js
+++
@@ -1,7 +0,0 @@
-function camelize(string) {
-  return string.replace(/-(.)/g, (_, character) => character.toUpperCase())
-}
-
-export default function convertSitepackIdToJunctionsKey(id) {
-  return camelize(id.replace(/\.[a-zA-Z\.]+$/, '').replace('.', '-').split('/').reverse()[0])
-}
